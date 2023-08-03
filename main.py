f194b9a7e1816ad14ec7247427478263ba2ce55a
Victor Robinson <victorrobinson2017@gmail.com>
2023-08-03 08:30:19 -0400

    Keep the window from closing

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ import time
 WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Jukey Spacey")
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
