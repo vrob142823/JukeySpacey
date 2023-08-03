63d2f0ab31faa456f0a45a2496688c28ad216f64
Victor Robinson <victorrobinson2017@gmail.com>
2023-08-03 08:44:40 -0400

    added bg to window and scaled it

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Jukey Spacey")
 
+BG = pygame.transform.scale(pygame.image.load("bg.jpeg"), (WIDTH, HEIGHT))
+
+def draw(): #add background image to the window
+    WIN.blit(BG, (0, 0))
+    pygame.display.update()
+
 def main():
     run = True
 
@@ -14,6 +20,8 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        draw()
 
     pygame.quit()
 
