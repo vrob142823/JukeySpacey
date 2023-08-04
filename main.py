0f8b45bfdc84d267f6ea5dbbb738ad9509f107b7
Victor Robinson <72229070+vrob142823@users.noreply.github.com>
2023-08-04 13:44:50 -0400

    added character and char. movement

--- main.py
+++ main.py
@@ -8,20 +8,37 @@ pygame.display.set_caption("Jukey Spacey")
 
 BG = pygame.transform.scale(pygame.image.load("bg.jpeg"), (WIDTH, HEIGHT))
 
-def draw(): #add background image to the window
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 60
+
+PLAYER_VEL = 5
+
+def draw(player): #add background image to the window
     WIN.blit(BG, (0, 0))
+
+    pygame.draw.rect(WIN, "white", player)
     pygame.display.update()
 
 def main():
     run = True
 
+    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
+    clock = pygame.time.Clock()
+
     while run:
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
 
-        draw()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >= 0:
+            player.x -= PLAYER_VEL
+        if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + player.width <= WIDTH:
+            player.x += PLAYER_VEL
+
+        draw(player)
 
     pygame.quit()
 
