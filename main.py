d154d2a7e8ed4606b4a5eb959cdc1c5e0cdd8232
Victor Robinson <72229070+vrob142823@users.noreply.github.com>
2023-08-07 22:49:17 -0400

    added projectiles

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ PLAYER_WIDTH = 40
 PLAYER_HEIGHT = 60
 
 PLAYER_VEL = 5
+STAR_WIDTH = 10
+STAR_HEIGHT = 15
 
 FONT = pygame.font.SysFont("comicsans", 30)
 
@@ -34,9 +36,23 @@ def main():
     start_time = time.time()
     elapsed_time = 0
 
+    star_add_increment = 2000
+    star_count = 0
+
+    star = []
+
     while run:
-        clock.tick(60)
+        star_count += clock.tick(60)
         elapsed_time = time.time() - start_time
+
+        if star_count > star_add_increment:
+            for _ in range(3):
+                star_x = random.randint(0, WIDTH - STAR_WIDTH)
+                star = pygame.Rect(star_x, -STAR_HEIGHT, STAR_WIDTH, STAR_HEIGHT)
+                stars.append(star)
+
+            star_add_increment = max(200, star_add_increment - 50)
+            star_count = 0
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
